cf68d4fcd7d9ffe0235807bcf24571b70f32f6c9
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-04 22:17:31 +0300

    implement initial mobile navigation dropdown component and styles

--- src/components/navigation/MobileNavigationDropdown.tsx
+++ src/components/navigation/MobileNavigationDropdown.tsx
@@ -1,4 +1,7 @@
+import { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
 import styles from './MobileNavigationDropdown.module.scss';
+import { CaretUp, CaretDown } from '@phosphor-icons/react';
 
 interface Props {
     title: string,
@@ -6,26 +9,43 @@ interface Props {
     selected: boolean,
     handleSelected: (category: string) => void,
 }
+
 function MobileNavigationDropdown({ title, items, selected, handleSelected }: Props) {
+    const { theme } = useContext(ThemeContext);
+    const [selectItem, setSelectItem] = useState('');
+
+    const handleSelectedLink = (itemName: string) => {
+        setSelectItem(itemName);
+    }
+
+    useEffect(() => {
+        if (!selected) {
+            setSelectItem('');
+        }
+    }, [selected]);
+
     return (
         <div
-            className={`${styles.MobileNavigationDropdown} ${selected ? styles.Selected : ''}`}
-            onClick={() => handleSelected(title)}
+            className={`${styles.MobileNavigationDropdown} ${selected ? styles.Selected : ''} ${styles[theme]}`}
+
         >
-            <h3 className={styles.MobileNavigationDropdownTitle}>
+            <h3 className={styles.MobileNavigationDropdownTitle} onClick={() => handleSelected(title)}>
                 {title}
-                {items && items.length ? <span className={styles.MobileNavigationDropdownPointer}>^</span> : ''}
+                {items && items.length ? (selected ? <CaretUp size={24} /> : <CaretDown size={24} />) : null}
             </h3>
             {selected && items && items.length
                 ?
-                items.map((value, key) => (
-                    <p
-                        key={key}
-                        className={styles.MobileNavigationDropdownItem}
-                    >
-                        {value}
-                    </p>
-                ))
+                <div className={styles.MobileNavigationDropdownLinksWrapper} >
+                    {items.map((value, key) => (
+                        <a
+                            key={key}
+                            className={`${styles.MobileNavigationDropdownLink} ${value === selectItem ? styles.SelectedLink : ''}`}
+                            onClick={() => handleSelectedLink(value)}
+                        >
+                            {value}
+                        </a>
+                    ))}
+                </div>
                 :
                 null
             }
