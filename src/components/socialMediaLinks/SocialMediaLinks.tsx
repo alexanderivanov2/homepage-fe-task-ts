b464cfbad0de89a779a6ed7fe5280f42fc129bb7
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-04-06 12:26:56 +0300

    refactor social media links to can be used in footer

--- src/components/socialMediaLinks/SocialMediaLinks.tsx
+++ src/components/socialMediaLinks/SocialMediaLinks.tsx
@@ -2,10 +2,15 @@ import { Youtube, Facebook } from 'lucide-react';
 import styles from './SocialMediaLinks.module.scss';
 import { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
-function SocialMediaLinks() {
+
+interface Props {
+    staticPositon?: boolean;
+}
+
+function SocialMediaLinks({ staticPositon }: Props) {
     const { theme } = useContext(ThemeContext);
     return (
-        <div className={`${styles.socialMediaLinks} ${styles[theme]}`}>
+        <div className={`${styles.socialMediaLinks} ${styles[theme]} ${ staticPositon && styles.staticPositon}`}>
             <div className={`${styles.socialLinkBox} ${styles.facebook}`}>
                 <Facebook size={22} />        
             </div>
